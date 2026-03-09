f70ba6b2268dbd9beb343ed50fb50f98609c3ac7
DH52200637-NguyenTriHao <haonguyen27042004@gmail.com>
2026-03-09 13:40:19 +0700

    user test read

--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -1,25 +1,17 @@
 <?php
-use App\Http\Controllers\shop\trangchuController;
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\ProductsController;
 use App\Http\Controllers\admin\PageController;
-use App\Models\Category;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\UserTestController;
+Route::prefix('admin')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('products', ProductsController::class);
+    Route::apiResource('pages', PageController::class);
+    Route::apiResource('users', UserTestController::class);
+});
 
-Route::get('/', [trangchuController::class, 'index'])
-    ->name('trangchu');
-
-Route::get('/shop/category/{id}', [trangchuController::class, 'show'])
-    ->name('shop.category');
-
-Route::get('/shop/search', [trangchuController::class, 'search'])
-    ->name('shop.search');
-
-Route::get('/shop/search/suggest', [trangchuController::class, 'searchSuggest'])
-    ->name('shop.searchsuggest');
-
-Route::get('/shop/chitietsanpham/{id}', [trangchuController::class, 'detail'])
-    ->name('shop.chitietsanpham');
-
-
-
+Route::prefix('BASE_API')->group(function () {
+    Route::apiResource('users', UserTestController::class);
+});
