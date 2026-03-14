4896375aa1a5035695e22019a14920f808f10526
NgocBich <ngocbich@gmail.com>
2026-03-14 15:02:24 +0700

    Fix lan 6

--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -6,11 +6,15 @@ use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\ProductsController;
 use App\Http\Controllers\admin\UserTestController;
 
+Route::get('/', function () {
+    return "Backend đã khởi động thành công";
+});
 
+Route::prefix('admin')->group(function () {
 
-    Route::resource('/', ProductsController::class);
+    Route::resource('products', ProductsController::class);
     Route::resource('categories', CategoryController::class);
-
+});
 
 Route::prefix('BASE_API')->group(function () {
     Route::apiResource('users', UserTestController::class);
