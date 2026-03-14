aec809608e6b6c2cd32ccf583a6d6f68e8f00c9b
NgocBich <ngocbich@gmail.com>
2026-03-14 13:10:04 +0700

    fix lan 5

--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -7,11 +7,10 @@ use App\Http\Controllers\admin\ProductsController;
 use App\Http\Controllers\admin\UserTestController;
 
 
-Route::prefix('admin')->group(function () {
 
-    Route::resource('products', ProductsController::class);
+    Route::resource('/', ProductsController::class);
     Route::resource('categories', CategoryController::class);
-});
+
 
 Route::prefix('BASE_API')->group(function () {
     Route::apiResource('users', UserTestController::class);
